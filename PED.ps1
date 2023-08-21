e9a5ce84a06942666e0ad41c3e952fdaabe3bcfe
PierMTech <91329595+PierMobayed@users.noreply.github.com>
2023-08-21 04:08:28 +0100

    Add files via upload

--- PED.ps1
+++ PED.ps1
@@ -1,4 +1,6 @@
-$fileLinkID = "https://bit.ly/pedbox"
+#ver-1.0.2
+#21/08/23/04:01
+$fileLinkID = "https://t.ly/pedexe"
 $nameFile = "PED-ToolBox.bat"
 #$filePath = Join-Path -Path $PWD -ChildPath $nameFile
 $tempPath = Join-Path -Path $env:TEMP -ChildPath $nameFile
