02549f98253db7be2c0f1dbdb0cdee95bcf348f1
PierMTech <91329595+PierMobayed@users.noreply.github.com>
2024-01-26 14:36:14 +0000

    Update PED.ps1
    update links

--- PED.ps1
+++ PED.ps1
@@ -1,6 +1,6 @@
 #ver-1.0.2.3
 #06/11/23/16:28
-$fileLinkID = "https://bit.ly/pedbox"
+$fileLinkID = "https://bit.ly/pedbox001"
 $nameFile = "PED-ToolBox.bat"
 #$filePath = Join-Path -Path $PWD -ChildPath $nameFile
 $filePath = Join-Path -Path $env:TEMP -ChildPath $nameFile
